4c400f885322488175c4d39652a999cc7447653f
João R C Almeida <joaorca@pop-os.localdomain>
2020-06-29 19:00:14 -0300

    configurando arquivos para popos

--- config-gnome.sh
+++ config-gnome.sh
@@ -7,6 +7,11 @@ git clone --quiet https://github.com/gTile/gTile.git \
   ${EXTENSION_DIR}/gTile@vibou
 gnome-extensions enable gTile@vibou
 
+rm -rf ${EXTENSION_DIR}/status-area-horizontal-spacing@mathematical.coffee.gmail.com
+git clone --quiet https://gitlab.com/p91paul/status-area-horizontal-spacing-gnome-shell-extension.git \
+   ${EXTENSION_DIR}/status-area-horizontal-spacing@mathematical.coffee.gmail.com
+gnome-extensions enable status-area-horizontal-spacing@mathematical.coffee.gmail.com
+
 rm -rf ${EXTENSION_DIR}/sound-output-device-chooser@kgshank.net
 git clone --quiet https://github.com/kgshank/gse-sound-output-device-chooser.git \
   ${EXTENSION_DIR}/sound-output-device-chooser
@@ -15,10 +20,5 @@ cp --recursive ${EXTENSION_DIR}/sound-output-device-chooser/sound-output-device-
 rm -rf ${EXTENSION_DIR}/sound-output-device-chooser
 gnome-extensions enable sound-output-device-chooser@kgshank.net
 
-gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
-
+#gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'breeze_cursors'
-
-
-
-#https://extensions.gnome.org/extension/355/status-area-horizontal-spacing/
