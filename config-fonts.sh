bb0f2dc28205a95d5b8b604bb1a2a720e21748b8
João R C Almeida <joaorca@gmail.com>
2021-03-23 09:55:29 -0300

    inclusao de font

--- config-fonts.sh
+++ config-fonts.sh
@@ -18,7 +18,6 @@ for type in Bold Light Medium Regular Retina; do
   fi;
 done
 
-
 for type in regular italic bold bold-italic light light-italic medium medium-italic condensed mono mono-italic mono-bold mono-bold-italic; do
   file_path="${fonts_dir}/Ubuntu-${type}.ttf"
   file_url="https://www.1001fonts.com/download/font/ubuntu.${type}.ttf"
@@ -29,6 +28,15 @@ for type in regular italic bold bold-italic light light-italic medium medium-ita
   fi;
 done
 
+for type in Bold Medium Light BoldItalic Light-1; do
+  file_path="${fonts_dir}/Ubuntu-${type}.ttf"
+  file_url="https://fontsfree.net//wp-content/fonts/basic/sans-serif/FontsFree-Net-OperatorMono-${type}.ttf"
+  if [ ! -e "${file_path}" ]; then
+    wget -q "${file_url}" -O "${file_path}" --show-progress
+  else
+    echo "Found existing file $file_path"
+  fi;
+done
 
 echo "fc-cache -f"
 fc-cache -f
